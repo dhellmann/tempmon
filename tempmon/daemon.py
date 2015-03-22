8160540662b887f910ece6a7e61f2a076e9b7046
Doug Hellmann <doug@doughellmann.com>
2015-03-22 09:12:08 -0400

    add OWM weather reporting and labels for traces

--- tempmon/daemon.py
+++ tempmon/daemon.py
@@ -6,9 +6,11 @@ import os
 import time
 
 import plotly.plotly as py
-from plotly.graph_objs import Scatter, Layout, Figure
-import yaml
+from plotly.graph_objs import Scatter, Layout, Figure, YAxis
+import pyowm
 from temperusb import TemperHandler
+import yaml
+
 
 # Measure every 5 minutes
 DEFAULT_FREQUENCY = 5
@@ -16,6 +18,92 @@ DEFAULT_FREQUENCY = 5
 DEFAULT_RETENTION_PERIOD = 7
 # Sorry, Europe
 DEFAULT_UNITS = 'fahrenheit'
+
+LOG = logging.getLogger('')
+
+
+def setup_logging(log_file, verbose):
+    # Configure logging to minimize disk space
+    LOG.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(
+        log_file,
+        maxBytes=4096,
+        backupCount=1,
+    )
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    LOG.addHandler(handler)
+    logging.captureWarnings(True)
+
+    if verbose:
+        out = logging.StreamHandler()
+        out.setFormatter(logging.Formatter('%(message)s'))
+        out.setLevel(logging.DEBUG)
+        LOG.addHandler(out)
+    else:
+        # Quiet the chatty requests module
+        logging.getLogger('requests').setLevel(logging.WARN)
+
+
+def get_sensors():
+    th = TemperHandler()
+    devs = th.get_devices()
+    if not devs:
+        raise RuntimeError('No temperature sensors found')
+    return devs
+
+
+def create_plot(username, api_key,
+                weather_token,
+                sensor_tokens, sensor_names,
+                title, units,
+                max_points):
+    py.sign_in(username, api_key)
+    traces = [
+        Scatter(
+            x=[],
+            y=[],
+            name=n,
+            stream={
+                'token': t,
+                'maxpoints': max_points,
+            }
+        )
+        for t, n in zip(sensor_tokens, sensor_names)
+    ]
+    traces.append(
+        Scatter(
+            x=[],
+            y=[],
+            name='Outside Temperature',
+            stream={
+                'token': weather_token,
+                'maxpoints': max_points,
+            }
+        )
+    )
+    layout = Layout(
+        title=title,
+        yaxis=YAxis(
+            title='Degrees %s' % units.title(),
+        ),
+    )
+    fig = Figure(data=traces, layout=layout)
+    LOG.info('Output graph visible at %s', py.plot(fig, filename=title))
+
+    sensor_streams = [
+        py.Stream(t)
+        for t in sensor_tokens
+    ]
+    for s in sensor_streams:
+        s.open()
+
+    weather_stream = py.Stream(weather_token)
+    weather_stream.open()
+
+    return sensor_streams, weather_stream
 
 
 def main():
@@ -46,96 +134,89 @@ def main():
         with open(args.pid_file, 'w') as f:
             f.write('%s\n' % os.getpid())
 
-    # Configure logging to minimize disk space
-    log = logging.getLogger('')
-    log.setLevel(logging.INFO)
-    handler = logging.handlers.RotatingFileHandler(
-        args.log_file,
-        maxBytes=4096,
-        backupCount=1,
-    )
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-    logging.captureWarnings(True)
+    setup_logging(args.log_file, args.verbose)
 
-    if args.verbose:
-        out = logging.StreamHandler()
-        out.setFormatter(logging.Formatter('%(message)s'))
-        out.setLevel(logging.DEBUG)
-        log.addHandler(out)
-    else:
-        # Quiet the chatty requests module
-        logging.getLogger('requests').setLevel(logging.WARN)
-
-    log.info('Loading configuration from %s', args.config_file)
+    LOG.info('Loading configuration from %s', args.config_file)
     with open(args.config_file) as f:
         config = yaml.load(f)
-    username = config['username']
-    api_key = config['api-key']
-    stream_tokens = config['stream-tokens']
+    plotly = config['plotly']
+    username = plotly['username']
+    api_key = plotly['api-key']
+    sensor_tokens = plotly['sensor-stream-tokens']
     title = config.get('graph-title', 'Temperature')
     retention_period = config.get('retention-period', DEFAULT_RETENTION_PERIOD)
     frequency = config.get('frequency', DEFAULT_FREQUENCY)
     if frequency < 1:
-        log.warning('Cannot poll more often than 1 minute')
+        LOG.warning('Cannot poll more often than 1 minute')
         frequency = 1
     units = config.get('units', DEFAULT_UNITS)
-    log.info('Polling every %s minutes, keeping %s days',
+    LOG.info('Polling every %s minutes, keeping %s days',
              frequency, retention_period)
+    weather_stream_token = plotly['weather-stream-token']
+    weather = config['weather']
+    owm_api_key = weather['api-key']
+    owm_place = weather['place']
+    LOG.info('Monitoring weather at %r', owm_place)
 
     # Make sure we can communicate with the devices
-    th = TemperHandler()
-    devs = th.get_devices()
-    if not devs:
-        raise RuntimeError('No temperature sensors found')
-    for dev in devs:
-        log.info('Found sensor on bus %s at port %s',
-                 dev.get_bus(), dev.get_ports())
+    devs = get_sensors()
+    sensor_names = []
+    if len(devs) > 1:
+        name_format = 'Sensor %(num)s (%(bus)s/%(ports)s)'
+    else:
+        name_format = 'Sensor'
+    for n, dev in enumerate(devs, 1):
+        bus = dev.get_bus()
+        ports = dev.get_ports()
+        LOG.info('Found sensor on bus %s at port %s',
+                 bus, ports)
+        name = name_format % {'bus': bus, 'ports': ports, 'num': n}
+        sensor_names.append(name)
+
+    # Connect to OWM weather service
+    owm = pyowm.OWM(owm_api_key)
+    observation = owm.weather_at_place(owm_place)
 
     # Make sure our plotly login details work
-    py.sign_in(username, api_key)
     max_points = 24 * (60 / frequency) * retention_period
-    traces = [
-        Scatter(
-            x=[],
-            y=[],
-            stream={
-                'token': t,
-                'maxpoints': max_points,
-            }
-        )
-        for t in stream_tokens
-    ]
-    layout = Layout(title=title)
-    fig = Figure(data=traces, layout=layout)
+    sensor_streams, weather_stream = create_plot(
+        username,
+        api_key,
+        weather_stream_token,
+        sensor_tokens,
+        sensor_names,
+        title,
+        units,
+        max_points,
+    )
 
-    streams = [
-        py.Stream(t)
-        for t in stream_tokens
-    ]
-    for s in streams:
-        s.open()
-
-    log.info('Output graph visible at %s', py.plot(fig, filename=title))
+    LOG.info('Starting polling')
     delay = frequency * 60
-
-    log.info('Starting polling')
     while True:
         x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-        for dev, stream in zip(devs, streams):
+        # Reported temperature from OWM
+        try:
+            w = observation.get_weather()
+            temp = w.get_temperature('fahrenheit')['temp']
+        except Exception:
+            LOG.warning('Could not get weather report', exc_info=True)
+        else:
+            try:
+                weather_stream.write({'x': x, 'y': temp})
+            except Exception:
+                LOG.warning('Could not update plotly', exc_info=True)
+        # Temperature sensors
+        for dev, stream in zip(devs, sensor_streams):
             try:
                 temp = dev.get_temperature(format=units)
             except Exception:
-                log.warning('Could not read temperature', exc_info=True)
-                break
+                LOG.warning('Could not read temperature', exc_info=True)
+                continue
             try:
                 stream.write({'x': x, 'y': temp})
             except Exception:
-                log.warning('Could not update plotly', exc_info=True)
-                break
+                LOG.warning('Could not update plotly', exc_info=True)
+                continue
         # delay between stream posts is expressed as a frequency
         # in minutes
         time.sleep(delay)
